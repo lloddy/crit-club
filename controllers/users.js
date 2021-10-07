65e5110a926413975ea1e2462f38fd801df27262
Lloyd Eddy <lloyd@lloyds-mbp.home>
2021-10-07 16:39:50 -0400

    WIP ejs pages created, tested, user model set-up

--- controllers/users.js
+++ controllers/users.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcrypt');
+const User = require('../models/user.js');
 
 // INDEX
-
+router.get('/', (req, res) => {
+    res.render('home.ejs')
+})
 // NEW
 
 // DElETE
